3876dd5df7e0ed2b0759f131a0fd90a40e6c3f5f
wddnguyen <wddnguyen@gmail.com>
2021-03-06 13:17:36 -0500

    Added room creation to redux store and clean up lobby component

--- src/components/Lobby.js
+++ src/components/Lobby.js
@@ -1,38 +1,29 @@
-import React, {useState} from 'react'
+import React from 'react'
 import {makeStyles} from '@material-ui/core/styles'
+import {useSelector, useDispatch} from 'react-redux'
+import {setNewRoomPlayerSide, setNewRoomName, setNewRoomOwner} from '../reducers/lobbyReducer'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemText from '@material-ui/core/ListItemText'
 import List from '@material-ui/core/List'
 import Grid from '@material-ui/core/Grid'
-import {useSelector, useDispatch} from 'react-redux'
-import {addRoom} from '../reducers/lobbyReducer'
-import { Typography } from '@material-ui/core'
+import {Typography} from '@material-ui/core'
 import Button from '@material-ui/core/Button'
 import TextField from '@material-ui/core/TextField'
 import FormControl from '@material-ui/core/FormControl'
 import Select from '@material-ui/core/Select'
 import InputLabel from '@material-ui/core/InputLabel'
-import SendIcon from '@material-ui/icons/Send';
+import AddBoxIcon from '@material-ui/icons/AddBox'
 
 const Lobby = () => {
     const dispatch = useDispatch()
-    const rooms = useSelector(state => state.lobby)
-    const [side, setSide] = useState('white')
+    const rooms = useSelector(state => state.lobby.rooms)
+    const newRoom = useSelector(state => state.lobby.newRoom)
 
     const useStyles = makeStyles((theme) => ({
-        paper: {
-          marginTop: theme.spacing(8),
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-        },
         form: {
           width: '100%', // Fix IE 11 issue.
           marginTop: theme.spacing(1),
           border: '2px solid black'
-        },
-        submit: {
-          margin: theme.spacing(3, 0, 2),
         },
         table: {
             border: '2px solid black'
@@ -53,65 +44,69 @@ const Lobby = () => {
 
     const handleCreateRoom = (event) => {
         event.preventDefault()
+        dispatch(setNewRoomOwner('player'))
     }
 
     const handleSideChange = (event) => {
-        setSide(event.target.value)
+        dispatch(setNewRoomPlayerSide(event.target.value))
+    }
+
+    const handleRoomNameChange = (event) => {
+        dispatch(setNewRoomName(event.target.value))
     }
 
     const classes = useStyles()
+
     return (
-            <Grid container justify="center" direction="column" alignItems="center">
-                <Grid item>
-                    <form className={classes.form} onSubmit={() => handleCreateRoom}>
-                        <TextField
-                            name="roomName"
-                            placeholder="roomName"
-                            label="Room Name"
-                        />
-                        <FormControl variant="outlined" className={classes.formControl}>
-                            <InputLabel htmlFor="outlined-age-native-simple">Side</InputLabel>
-                            <Select
+        <Grid container justify="center" direction="column" alignItems="center">
+            <Grid item>
+                <form className={classes.form} onSubmit={handleCreateRoom}>
+                    <TextField
+                        name="roomName"
+                        placeholder="roomName"
+                        label="Room Name"
+                        onChange={handleRoomNameChange}
+                        value={newRoom.roomName}
+                    />
+                    <FormControl variant="outlined" className={classes.formControl}>
+                        <InputLabel htmlFor="outlined-age-native-simple">Side</InputLabel>
+                        <Select
                             native
-                            value={side}
+                            value={newRoom.side}
                             onChange={handleSideChange}
                             label="Side"
-                            inputProps={{
-                                name: 'age',
-                                id: 'outlined-age-native-simple',
-                            }}
-                            >
-                                <option value='white'>White</option>
-                                <option value='black'>Black</option>    
-                            </Select>
-                        </FormControl> 
-                        <Button 
-                            variant="contained"
-                            color="primary" 
-                            endIcon={<SendIcon />}
-                            type="submit"
                         >
-                            create
-                        </Button>          
-                    </form>
-                </Grid>
-                <Grid item>
-                    <Typography variant="h4">
-                        Available Rooms
-                    </Typography>
-                </Grid>
-                <Grid item xs={6} className={classes.table}>
-                    <List className={classes.list}>
-                        {rooms.map(room => {
-                            return (
-                                <ListItem key={room.id} button onClick={() => handleJoinGame(room)}>
-                                    <ListItemText primary={room.name} secondary={room.player}/>
-                                </ListItem>
-                            )
-                        })}
-                    </List>
-                </Grid>
+                            <option value='white'>White</option>
+                            <option value='black'>Black</option>    
+                        </Select>
+                    </FormControl> 
+                    <Button 
+                        variant="contained"
+                        color="primary" 
+                        endIcon={<AddBoxIcon />}
+                        type="submit"
+                    >
+                        create
+                    </Button>          
+                </form>
             </Grid>
+            <Grid item>
+                <Typography variant="h4">
+                    Available Rooms
+                </Typography>
+            </Grid>
+            <Grid item xs={6} className={classes.table}>
+                <List className={classes.list}>
+                    {rooms.map(room => {
+                        return (
+                            <ListItem key={room.id} button onClick={() => handleJoinGame(room)}>
+                                <ListItemText primary={room.name} secondary={room.player}/>
+                            </ListItem>
+                        )
+                    })}
+                </List>
+            </Grid>
+        </Grid>
     )
 }
 
--- src/reducers/lobbyReducer.js
+++ src/reducers/lobbyReducer.js
@@ -1,16 +1,52 @@
-const lobbyReducer = (lobby = [], action) => {
+const lobbyReducer = (lobby = {rooms: [], newRoom: {roomName: '', roomOwner: '', side: 'white'}}, action) => {
     switch (action.type) {
-        case 'ADD_ROOM':
-            return lobby.concat(action.room)
+        case 'SET_NEW_ROOM_PLAYER_SIDE':
+            return {
+                ...lobby,
+                newRoom: {
+                    ...lobby.newRoom,
+                    side: action.side
+                }
+            }
+        case 'SET_NEW_ROOM_NAME':
+            return {
+                ...lobby,
+                newRoom: {
+                    ...lobby.newRoom,
+                    roomName: action.roomName
+                }
+            }
+        case 'SET_NEW_ROOM_OWNER':
+            return {
+                ...lobby,
+                newRoom: {
+                    ...lobby.newRoom,
+                    roomOwner: action.roomOwner
+                }
+            }
         default:
             return lobby
     }
 }
 
-export const addRoom = room => {
+export const setNewRoomPlayerSide = side => {
     return {
-        type: 'ADD_ROOM',
-        room: room
+        type: 'SET_NEW_ROOM_PLAYER_SIDE',
+        side: side
+    }
+}
+
+export const setNewRoomName = roomName => {
+    return {
+        type: 'SET_NEW_ROOM_NAME',
+        roomName: roomName
+    }
+}
+
+export const setNewRoomOwner = owner => {
+    return {
+        type: 'SET_NEW_ROOM_OWNER',
+        roomOwner: owner
     }
 }
 
